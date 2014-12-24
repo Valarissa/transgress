8764176233d1404d0de8cc80705416932ac703b8
RSid <sm1th303@gmail.com>
2014-12-24 15:01:37 -0500

    passing tests for all validator regex methods

--- spec/unit_tests/validator_spec.rb
+++ spec/unit_tests/validator_spec.rb
@@ -6,6 +6,10 @@ describe Validator do
     uri = "http://www.autostraddle.com"
     @content = Nokogiri::HTML(Request.get(uri)).root.content
     @content.encode!('UTF-8', 'UTF-8', :invalid => :replace)
+
+    @safe_terms = 5
+    @flagged_terms = 2
+    @redemption_terms = 2
   end
 
   describe "#new" do
@@ -14,9 +18,24 @@ describe Validator do
     end
 
     it "regexes includes all safe and flagged terms" do
-       current_count_of_safe_and_flagged_terms = 7
+       current_count_of_safe_and_flagged_terms = @safe_terms + @flagged_terms
        regexps = Validator.regexes
        regexps.length.should eql current_count_of_safe_and_flagged_terms
+    end
+
+    it "safe regexes includes all safe terms" do
+      regexps = Validator.safe_regexes
+      regexps.length.should eql @safe_terms
+    end
+
+    it "flagged regexes includes all flagged terms" do
+      regexps = Validator.flagged_regexes
+      regexps.length.should eql @flagged_terms
+    end
+
+    it "redemption regexes includes all safe terms" do
+      regexps = Validator.redemption_regexes
+      regexps.length.should eql @redemption_terms
     end
   end
 end
