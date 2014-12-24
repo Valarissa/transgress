073acfdcf5946bdbe74a142b33e4923b7d441c65
RSid <sm1th303@gmail.com>
2014-12-24 13:26:23 -0500

    passing unit test for instantiating a page object

---
+++ spec/unit_tests/page_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require 'nokogiri'
+
+describe Page do
+  before :each do
+    @page = Page.new "http://www.autostraddle.com"
+  end
+
+  describe "#new" do
+    it "takes one parameter and returns a Page object" do
+      @page.should be_an_instance_of Page
+    end
+  end
+
+end
